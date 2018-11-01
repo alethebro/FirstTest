2bdfa418ad4089243c0892783f2ced4be23b9225
alethebro <alexius12@hotmail.com>
2018-11-01 20:07:36 +0100

    Initial commit

---
+++ FirstTest/src/it/alex/first/Saluto.java
@@ -0,0 +1,23 @@
+package it.alex.first;
+
+public class Saluto {
+
+	String greeting="Ciao ciao";
+	
+	
+	public static void main(String[] args) {
+		Saluto s1=new Saluto();
+		Saluto s2=new Saluto("alex");
+
+	}
+	
+	public Saluto() {
+		System.out.println(greeting);
+	}
+	
+	public Saluto(String name) {
+		System.out.println(greeting + " " + name + "!!!");
+	}
+	
+
+}
